5dcef3e03529d961bcffcf8cbafa088bed269475
GhastCMD <kraken32bin@protonmail.com>
2022-06-20 20:46:17 -0300

    Added script to rename dataset files to a better format

---
+++ rename_files.py
@@ -0,0 +1,16 @@
+import os
+
+if __name__ == '__main__':
+    base_folders = [
+        './dataset/Base IADD/Atypical',
+        './dataset/Base IADD/Indeterminate',
+        './dataset/Base IADD/Typical',
+    ]
+    for base_folder in base_folders:
+        for file in os.listdir(base_folder):
+            if os.path.isfile(os.path.join(base_folder, file)):
+                name, ext = file.split('.')
+                os.rename(
+                    os.path.join(base_folder, file),
+                    os.path.join(base_folder, f'{name:0>4}.{ext}')
+                )
